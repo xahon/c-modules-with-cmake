a9fe7978c3a80990169f107c14086b42d7867f82
Ilya Chernikov <chernikov.i123@gmail.com>
2021-08-29 19:30:00 +0700

    Add basic C++20 modules code with cmake

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <iostream>
+import math;
+
+int main() {
+  std::cout << "Expects: 7, Result: " << add(5, 2) << std::endl;
+  std::cout << "Expects: 3, Result: " << subtract(5, 2) << std::endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ src/math.ixx
@@ -0,0 +1,13 @@
+module;
+#include <iostream>
+export module math;
+
+export int add(int a, int b)
+{
+    return a + b;
+}
+
+export int subtract(int a, int b)
+{
+    return a - b;
+}
